76d4410788f9754268c1a5d0e376e3ded14129b9
om <ogaikwad5656@gmail.com>
2025-07-23 16:55:32 +0530

    Add files via upload

---
+++ pratical 7.py
@@ -0,0 +1,11 @@
+marks =float(input("enter your marks:"))
+
+if marks >=101:
+    print("invalid input")
+
+elif marks >90:
+    print("grade A")
+
+elif marks >75:
+    print("grade b")
+            
